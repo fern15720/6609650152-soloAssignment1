8c69f58ef6b0bf89d01fe5809a87a11709ca8f23
Meow แงว <kanokporn.phan@dome.tu.ac.th>
2024-10-10 19:51:02 +0700

    initial login web

---
+++ public/js/script.js
@@ -0,0 +1,36 @@
+function submitLogin() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Application-Key' : 'TUcb24c31d468134b9d239d8465aebb11e3bad7796a3f0d9883cc665705c89006615ffbc4dd629091a23b613043869a0e8'
+        },
+        body: JSON.stringify({"UserName" : "username","PassWord" : "password"})
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('message').innerText = data.message;
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+
+
+function call_REST_API_Hello() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const url = (
+        'http://localhost:3000/hello?' +
+        new URLSearchParams({ myName: username, lastName: password}).toString()
+      );
+    
+    fetch(url)
+    .then(data => {
+        document.getElementById('message').innerText = data.message;
+    })
+    .catch(error => console.error('Error:', error));
+}

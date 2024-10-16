c42ca00d76c23f4e17d9e95cdef4cbb7e1a0ded6
Meow แงว <kanokporn.phan@dome.tu.ac.th>
2024-10-16 11:28:42 +0700

    add fuction disabled state until all fields are filled

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,27 @@
+
+const usernameField = document.getElementById("username");
+const passwordField = document.getElementById("password");
+const roleField = document.getElementById("role");
+const loginButton = document.getElementById("loginButton");
+
+function checkFields() {
+  if (usernameField.value.trim() !== "" && passwordField.value.trim() !== "" && roleField.value  !== "") {
+    loginButton.disabled = false; 
+  } else {
+    loginButton.disabled = true; 
+  }
+}
+
+usernameField.addEventListener("input", checkFields);
+passwordField.addEventListener("input", checkFields);
+roleField.addEventListener("change", checkFields);
+
 function submitLogin() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const username = usernameField.value;
+    const password = passwordField.value;
+
+    /*const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;*/
 
     fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
         method: 'POST',
@@ -12,9 +33,11 @@ function submitLogin() {
     })
     .then(response => response.json())
     .then(data => {
-        document.getElementById('message').innerText = data.message;
+      document.getElementById('message').innerText = data.message;
     })
     .catch(error => console.error('Error:', error));
+    
+    
 }
 
 function togglePassword() {
